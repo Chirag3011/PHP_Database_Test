7eb1d74483a04758b8fb89f78bc5b1956307aca8
chiragxx1sharma <Chirag.Sharma@noventiq.com>
2024-03-13 23:48:14 +0530

    C02: connection changes

--- connect.php
+++ connect.php
@@ -12,12 +12,12 @@ if ($conn->connect_error) {
     echo "$conn->connect_error";
     die("Connection Failed : " . $conn->connect_error);
 } else {
-    // $stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-    // $stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
-    // $execval = $stmt->execute();
-    // echo $execval;
-    // echo "Registration successfully...";
-    // $stmt->close();
+    $stmt = $conn->prepare("INSERT INTO registration (firstName, lastName, gender, email, password, number) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+    $execval = $stmt->execute();
+    echo $execval;
+    echo "Registration successfully...";
+    $stmt->close();
     $conn->close();
 }
 ?>
